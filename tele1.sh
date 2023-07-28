3c51eea0044a38132f4a4456b8b7ab8d1e0b0dda
Jordan Godwin <jordan.godwin13@gmail.com>
2023-07-27 21:48:46 -0500

    Add handy variable to choose sub directory in working directory.

--- tele1.sh
+++ tele1.sh
@@ -1,7 +1,12 @@
 #!/bin/zsh
 #just a cutie patootie little script to make my morning commute faster
+echo "where to, bruh?"
+ls PATH/TO/BIZ
 
-cd PATH/TO/BIZ
+read directory
+
+
+cd PATH/TO/BIZ/$directory
 
 echo "Sayonara, mf."
 echo "Welcome to YOUR BIZ"
